631501013c0127f28c22458ff733817eda2f7075
muhrizky-a <muhrizky1337a@gmail.com>
2026-01-22 16:05:20 +0800

    feat: crud product

--- main.go
+++ main.go
@@ -9,13 +9,130 @@ import (
 	"strings"
 )
 
+var products = []domain.Product{
+	{ID: 1, Name: "Indomie Godog", Price: 3500, Stock: 10},
+	{ID: 2, Name: "Vit 1000ml", Price: 3000, Stock: 40},
+	{ID: 3, Name: "kecap", Price: 12000, Stock: 20},
+}
+
 var categories = []domain.Category{
 	{ID: 1, Name: "Electronics", Description: "Electronics goods"},
 	{ID: 2, Name: "Cosmetics", Description: "Cosmetic goods"},
 }
 
+// POST localhost:8080/api/products
+func AddProduct(w http.ResponseWriter, r *http.Request) {
+	// baca data dari request
+	var newProduct domain.Product
+	err := json.NewDecoder(r.Body).Decode(&newProduct)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	// append products with the new data
+	newProduct.ID = len(products) + 1
+	products = append(products, newProduct)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201
+	json.NewEncoder(w).Encode(newProduct)
+}
+
+// GET localhost:8080/api/products
+func GetProducts(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
+
+// GET localhost:8080/api/products/{id}
+func GetProductByID(w http.ResponseWriter, r *http.Request) {
+	// Parse ID from URL path e.g. /api/products/123 -> ID = 123
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/products/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid Product ID", http.StatusBadRequest)
+		return
+	}
+
+	// lookup product by ID with linear search
+	for _, product := range products {
+		if product.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(product)
+			return
+		}
+	}
+
+	// Return if product not found
+	http.Error(w, "Product not found", http.StatusNotFound)
+}
+
+// PUT localhost:8080/api/products/{id}
+func UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	/// Parse ID from URL path e.g. /api/products/123 -> ID = 123
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/products/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid Product ID", http.StatusBadRequest)
+		return
+	}
+
+	// get data from request body
+	var updateProduct domain.Product
+	err = json.NewDecoder(r.Body).Decode(&updateProduct)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	// lookup product by ID with linear search, then update the found product's data
+	for i := range categories {
+		if categories[i].ID == id {
+			updateProduct.ID = id
+			products[i] = updateProduct
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(updateProduct)
+			return
+		}
+	}
+
+	// Return if products not found
+	http.Error(w, "Product not found", http.StatusNotFound)
+}
+
+// DELETE localhost:8080/api/products/{id}
+func DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	/// Parse ID from URL path e.g. /api/products/123 -> ID = 123
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/products/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid Product ID", http.StatusBadRequest)
+		return
+	}
+
+	// lookup product by ID with linear search, then remove the product
+	for i, product := range products {
+		if product.ID == id {
+			// bikin slice baru dengan data sebelum dan sesudah index
+			// Create the new slice with data before index(es) and data after index(es)
+			products = append(products[:i], products[i+1:]...)
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(map[string]string{
+				"message": "delete product success",
+			})
+			return
+		}
+	}
+
+	// Return if product not found
+	http.Error(w, "Product not found", http.StatusNotFound)
+}
+
 // POST localhost:8080/categories
-func AddCategories(w http.ResponseWriter, r *http.Request) {
+func AddCategory(w http.ResponseWriter, r *http.Request) {
 	// baca data dari request
 	var newCategory domain.Category
 	err := json.NewDecoder(r.Body).Decode(&newCategory)
@@ -41,7 +158,7 @@ func GetCategories(w http.ResponseWriter) {
 
 // GET localhost:8080/categories/{id}
 func GetCategoryByID(w http.ResponseWriter, r *http.Request) {
-	// Parse ID drom URL path e.g. /categories/123 -> ID = 123
+	// Parse ID from URL path e.g. /categories/123 -> ID = 123
 	idStr := strings.TrimPrefix(r.URL.Path, "/categories/")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
@@ -64,7 +181,7 @@ func GetCategoryByID(w http.ResponseWriter, r *http.Request) {
 
 // PUT localhost:8080/categories/{id}
 func UpdateCategory(w http.ResponseWriter, r *http.Request) {
-	/// Parse ID drom URL path e.g. /categories/123 -> ID = 123
+	/// Parse ID from URL path e.g. /categories/123 -> ID = 123
 	idStr := strings.TrimPrefix(r.URL.Path, "/categories/")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
@@ -98,7 +215,7 @@ func UpdateCategory(w http.ResponseWriter, r *http.Request) {
 
 // DELETE localhost:8080/categories/{id}
 func DeleteCategory(w http.ResponseWriter, r *http.Request) {
-	/// Parse ID drom URL path e.g. /categories/123 -> ID = 123
+	/// Parse ID from URL path e.g. /categories/123 -> ID = 123
 	idStr := strings.TrimPrefix(r.URL.Path, "/categories/")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
@@ -137,6 +254,44 @@ func main() {
 		},
 	)
 
+	http.HandleFunc("/api/products",
+		func(w http.ResponseWriter, r *http.Request) {
+			if r.Method == "POST" {
+				AddProduct(w, r)
+			} else if r.Method == "GET" {
+				GetProducts(w)
+			} else {
+				http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			}
+		},
+	)
+
+	http.HandleFunc("/api/products/",
+		func(w http.ResponseWriter, r *http.Request) {
+			if r.Method == "GET" {
+				GetProductByID(w, r)
+			} else if r.Method == "PUT" {
+				UpdateProduct(w, r)
+			} else if r.Method == "DELETE" {
+				DeleteProduct(w, r)
+			} else {
+				http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			}
+		},
+	)
+
+	http.HandleFunc("/categories",
+		func(w http.ResponseWriter, r *http.Request) {
+			if r.Method == "POST" {
+				AddCategory(w, r)
+			} else if r.Method == "GET" {
+				GetCategories(w)
+			} else {
+				http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			}
+		},
+	)
+
 	http.HandleFunc("/categories/",
 		func(w http.ResponseWriter, r *http.Request) {
 			if r.Method == "GET" {
@@ -145,16 +300,8 @@ func main() {
 				UpdateCategory(w, r)
 			} else if r.Method == "DELETE" {
 				DeleteCategory(w, r)
-			}
-		},
-	)
-
-	http.HandleFunc("/categories",
-		func(w http.ResponseWriter, r *http.Request) {
-			if r.Method == "GET" {
-				GetCategories(w)
-			} else if r.Method == "POST" {
-				AddCategories(w, r)
+			} else {
+				http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 			}
 		},
 	)
--- readme.md
+++ readme.md
@@ -1,6 +1,24 @@
 # Cashier API
 
-## Testing (Windows)
+## Testing Product (Windows)
+
+curl http://localhost:8080/health
+
+curl http://localhost:8080/api/products
+
+curl -X POST http://localhost:8080/api/products ^
+-H "Content-Type: application/json" ^
+-d "{ \"name\": \"Bag\", \"price\": 50000, \"stock\": 42 }"
+
+curl http://localhost:8080/api/products
+
+curl -X PUT http://localhost:8080/api/products/2 ^
+-H "Content-Type: application/json" ^
+-d "{ \"name\": \"Flowers\", \"price\": 42000, \"stock\": 41 }"
+
+curl -X DELETE http://localhost:8080/api/products/1
+
+## Testing Category (Windows)
 
 curl http://localhost:8080/health
 
@@ -8,16 +26,12 @@ curl http://localhost:8080/categories
 
 curl -X POST http://localhost:8080/categories ^
 -H "Content-Type: application/json" ^
--d "{
-\"name\": \"Sports\",
-\"description\": \"Sport goods\" }"
+-d "{ \"name\": \"Sports\", \"description\": \"Sport goods\" }"
 
 curl http://localhost:8080/categories
 
 curl -X PUT http://localhost:8080/categories/2 ^
 -H "Content-Type: application/json" ^
--d "{
-\"name\": \"Toys\",
-\"description\": \"Toys for kids\" }"
+-d "{ \"name\": \"Toys\", \"description\": \"Toys for kids\" }"
 
 curl -X DELETE http://localhost:8080/categories/1
